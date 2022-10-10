dc5b2a6d6aed54991cbb65f1332eef548da59fb5
jakhongirkholikulov <98052598+jahakholikulov002@users.noreply.github.com>
2022-10-11 00:39:03 +0500

    components post and progrees-bar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
   <div id="app">
     <main>
-      <header>
-        <h1>Jakhongir Kholikulov</h1>
-      </header>
-      <nav>
-        <a href="#">About Us</a>
-        <a href="#">Contact</a>
-        <a href="#">Portfolio</a>
-      </nav>
+      <div class="navigation">
+        <header>
+          <h1>Jakhongir Kholikulov</h1>
+        </header>
+        <nav>
+          <a href="#">About Us</a>
+          <a href="#">Contact</a>
+          <a href="#">Portfolio</a>
+        </nav>
+      </div>
+      <Progressbar />
+      <Post />
     </main>
 
   </div>
 </template>
 
 <script>
+import Post from '@/components/Post';
+import Progressbar from '@/components/Progressbar'
 export default {
   name: 'App',
 
+  components:{
+    Post,
+    Progressbar,
+  }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: 'monospace', sans-serif;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+.navigation {
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #000;
+  padding: 30px;
+}
+header h1{
+  color: #fff;
+  font-size: 32px;
+  /* text-align: center; */
+}
+nav a{
+  margin: 0 20px;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #fff;
+}
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,94 @@
+<template>
+    <article>
+        <div class="content">
+            <h2>Euro 2022</h2>
+            <h3>Final (Italy and England)</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque modi deserunt fuga ex sunt quo ratione
+                consequatur, in odit? Laudantium, ipsam ut, alias pariatur quis impedit repellendus accusamus dolorum in
+                perferendis tempore deleniti officiis cumque nobis! Voluptatem quisquam similique delectus possimus
+                molestiae officia inventore quo, quis tenetur facere. Quisquam, mollitia sint doloremque sed libero
+                provident porro explicabo animi officiis cupiditate iste eius ea est neque in consequatur vero aperiam
+                voluptatum. Cupiditate dicta earum temporibus molestias totam enim sed vero odio.</p>
+            <p>Corporis blanditiis doloremque ducimus voluptates illo quisquam in doloribus maxime? Facere eum sapiente
+                explicabo non iste enim dolore praesentium accusamus ratione est asperiores, in illo quaerat molestiae
+                reprehenderit reiciendis ipsa maxime quisquam distinctio nobis doloribus. Aliquam voluptate sunt
+                quibusdam officiis consequatur deleniti optio molestias itaque nobis provident tenetur, non eveniet
+                corrupti possimus nihil quia iure eius quod deserunt tempora? Voluptate ex incidunt dicta recusandae
+                enim! Repudiandae ea suscipit id, ex saepe sed inventore facere, quasi deserunt dolor sunt, eum
+                officiis?</p>
+            <p>Deserunt quidem sint sunt mollitia, nobis fugiat quas nemo amet modi animi ut excepturi reiciendis velit
+                ea sit eveniet voluptas odio cupiditate esse. Nesciunt ea in beatae inventore. Fugit necessitatibus
+                debitis eveniet iusto deserunt dignissimos quisquam itaque. Illo, libero reprehenderit asperiores
+                voluptatem error, facere natus quisquam a, iure quam ab aliquid sit dolorem. Temporibus, doloremque.
+                Consectetur corrupti necessitatibus minus error rerum eius sunt impedit, veritatis neque ipsam eveniet,
+                vel, atque voluptatum non! Similique totam dolores iste? Repellat esse quaerat eius.</p>
+            <p>Officia facere enim ex qui incidunt necessitatibus culpa pariatur quia itaque minus sunt, ut error,
+                dignissimos architecto. Quidem delectus fugit consequatur illo culpa aperiam ratione cumque illum
+                deserunt non, reprehenderit, rem, sequi qui quia dolores nemo earum! Officia, beatae sunt fugit omnis
+                vel cum voluptate accusamus consectetur qui quo, ad vero neque amet, minima ipsa quis ex. Temporibus
+                iusto quo doloremque adipisci eius. Laudantium sed eligendi voluptas ratione maiores, quae commodi
+                consequuntur, quas voluptatem assumenda corrupti ducimus? Deserunt, obcaecati hic.</p>
+            <p>Provident reprehenderit fugiat eius sunt repellendus debitis magni quibusdam voluptatibus asperiores
+                ipsum, illum distinctio non veniam voluptates blanditiis a minus. Ipsum laboriosam, minima quam autem
+                eveniet reiciendis officiis odio tempore atque necessitatibus nemo officia eos harum natus debitis ut
+                soluta. Distinctio facere repudiandae provident est, impedit ea deleniti odio quisquam sit eveniet.
+                Consectetur provident vitae, maxime reprehenderit quisquam aspernatur pariatur deserunt, dolores
+                laudantium voluptatibus nam ea facilis odio consequuntur necessitatibus amet! Odit, alias molestiae
+                nesciunt tempore tempora fuga fugit suscipit!</p>
+            <p>Tenetur accusantium culpa adipisci voluptates ipsam officia, nesciunt quaerat magnam in qui earum odio
+                porro perferendis totam, eaque dignissimos ad ipsum placeat nihil, corrupti nostrum ratione est! Nemo
+                quasi exercitationem dicta alias laudantium consequuntur minima error voluptatibus dolorem nisi aliquam
+                est blanditiis, molestiae ducimus delectus aliquid laborum? Obcaecati et tempora alias nihil eaque ipsum
+                at ipsam voluptas dignissimos? Dolor quod ex dolore eos ipsum et velit, iure ab, aliquam ea possimus,
+                dolorum sapiente necessitatibus error aut consequatur at accusamus maxime!</p>
+            <p>Quia omnis quas numquam, ipsum aut, possimus voluptate corrupti aperiam, reprehenderit rem provident
+                vitae illum eum distinctio. Quasi minima perspiciatis nesciunt a voluptatem sit qui eaque tenetur,
+                voluptatum ad, quidem maiores veritatis. Delectus itaque accusamus nostrum sit, suscipit veritatis cum
+                dolorum temporibus totam eligendi reprehenderit saepe repellat unde expedita, mollitia iste perferendis
+                quam enim optio? Officiis, a voluptas, quia dolorem voluptatibus debitis mollitia adipisci ex corporis
+                perferendis quisquam quos placeat quidem quod. Quas rem mollitia eaque. Quod, maxime nulla. Error!</p>
+            <p>Necessitatibus, optio eum, ullam nesciunt non ab aliquam dignissimos provident distinctio sint et est
+                placeat voluptates. Doloribus, corporis! Maiores in ratione quaerat eligendi accusantium reiciendis non
+                ab laborum veniam fugit. In magnam, dicta quae iste nostrum quia id ipsa, odio perferendis totam autem
+                libero, fugit maxime nam rerum dolore iusto delectus quaerat deleniti doloremque provident nihil
+                accusamus. Labore, numquam vitae? Labore doloremque, possimus soluta beatae doloribus natus quidem? Ad
+                hic libero maiores quod corrupti amet quis illum repellat aperiam deserunt.</p>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'post'
+}
+</script>
+
+<style>
+article {
+    width: 100%;
+    flex: 1 1 100%;
+    max-height: calc(100vh - 80px);
+    background: crimson;
+    overflow: scroll;
+}
+article .content{
+    padding: 25px;
+}
+article .content h2{
+    font-size: 32px;
+    font-weight: 900;
+    margin-bottom: 10px;
+}
+article .content h3{
+    font-size: 20px;
+    font-weight: 300;
+    margin-bottom: 30px;
+}
+article .content p{
+    color: #313131 ;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 1.5;
+    margin-bottom: 15px;
+}
+</style>
